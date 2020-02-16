5312e813af8598328da7ca711bcaebd0ae270896
n.ozgurozturk <ozgur@nozgurozturk.com>
2020-02-16 17:16:05 +0300

    Fix missing token issue

--- src/utils/context.js
+++ src/utils/context.js
@@ -1,13 +1,15 @@
 const token = require('./token')
 const User = require('../models/user')
 
-const TOKEN_HEADER_NAME = 'x-token'
+// const TOKEN_HEADER_NAME = 'x-token'
 
 const getUser = async req => {
   if (!req) return null
 
-  const tokenHeader = req.get(TOKEN_HEADER_NAME)
-  if (!tokenHeader) return null
+  if (!req.headers.authorization) return 'Bearer Token is not found'
+
+  const tokenHeader = req.headers.authorization.split(' ')[1]
+  if (!tokenHeader) return 'Token is not found'
 
   try {
     const decodedToken = await token.decode(tokenHeader)

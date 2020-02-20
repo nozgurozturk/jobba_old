aee49745e218ef345b145021ef47ee783c53485b
n.ozgurozturk <ozgur@nozgurozturk.com>
2020-02-20 03:16:51 +0300

    Update(scrapper) : base request with headers

--- src/modules/job/scrapper/base.js
+++ src/modules/job/scrapper/base.js
@@ -8,8 +8,26 @@ class BaseScrapper {
 
   async $ () {
     try {
-      const html = await rp(this.link)
+      // const html = rp({
+      //   uri: 'https://www.stackoverflow.com/',
+      //   method: 'GET',
+      //   resolveWithFullResponse: true
+      // })
+      const html = await rp({
+        uri: this.link,
+        rejectUnauthorized: false,
+        headers: {
+          accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+          'accept-language': 'en-US,en;q=0.8,ms;q=0.6',
+          'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
+        }
+      }, function (err, response, body) {
+        if (err) throw new Error('Not redirected')
+        return body
+      })
+
       if (!html) throw new Error('HTML is empty')
+
       const DOM = cheerio.load(html)
 
       DOM.prototype.cleaner = function () {
@@ -22,4 +40,5 @@ class BaseScrapper {
     }
   }
 }
+
 module.exports = BaseScrapper
